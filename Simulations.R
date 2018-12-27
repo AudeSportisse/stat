e47badeab6decfcf3a4f2a898fafe1bbffdc6f81
AudeSportisse <45034373+AudeSportisse@users.noreply.github.com>
2018-12-27 10:01:40 +0100

    Update Simulations.R

--- Simulations.R
+++ Simulations.R
@@ -10,13 +10,13 @@ r=1
 a=3
 b=0
 nbsim=50
-bruit=0.8
+noise=0.8
 Ns=500
 dat=rnorm(n)
 Xtrue=cbind.data.frame(dat,dat*1.1,dat*1.2,dat*1.3)
-resultProbitUnivariate=ComparMNAR_Univariate(Xtrue,a,b,r,bruit,Ns,"probit","MNAR",nbsim)
-resultUnivariate=ComparMNAR_Univariate(Xtrue,a,b,r,bruit,Ns,"logit","MNAR",nbsim)
-resultMARUnivariate=ComparMNAR_Univariate(Xtrue,a,b,r,bruit,Ns,"logit","MAR",nbsim)
+resultProbitUnivariate=ComparMNAR_Univariate(Xtrue,a,b,r,noise,Ns,"probit","MNAR",nbsim) #probit, MNAR
+resultUnivariate=ComparMNAR_Univariate(Xtrue,a,b,r,noise,Ns,"logit","MNAR",nbsim) #logit, MNAR
+resultMARUnivariate=ComparMNAR_Univariate(Xtrue,a,b,r,noise,Ns,"logit","MAR",nbsim) #logit, MAR
 
 
 ######
@@ -33,12 +33,12 @@ b=0
 a2=2
 b2=1
 nbsim=50
-bruit=0.8
+noise=0.8
 Ns=500
-res=simu(n,p,r,sqrt(bruit))
+res=simu(n,p,r,sqrt(noise))
 Xtrue=res$mu
-resultMARBivariate=ComparMNAR_Bivariate(Xtrue,a,b,a2,b2,r,bruit,Ns,colbis=4,m1=10,m2=20,nbsim) #MAR mechanism
-resultMNARBivariate=ComparMNAR_Bivariate(Xtrue,a,b,a2,b2,r,bruit,Ns,colbis=4,m1=1,m2=4,nbsim) #MNAR mechnanism
+resultMARBivariate=ComparMNAR_Bivariate(Xtrue,a,b,a2,b2,r,noise,Ns,colbis=4,m1=10,m2=20,nbsim) #MAR mechanism
+resultMNARBivariate=ComparMNAR_Bivariate(Xtrue,a,b,a2,b2,r,noise,Ns,colbis=4,m1=1,m2=4,nbsim) #MNAR mechnanism
 
 #######
 #Multivariate
@@ -53,7 +53,7 @@ r=4
 a=3
 b=0
 nbsim=50
-bruit=0.5 
+noise=0.5 
 Ns=500
-Xtrue=simu(n,p,r,sqrt(bruit))$mu
-result2=ComparMNAR_Multivariate(Xtrue,a,b,r,bruit,Ns,10,nbsim)
+Xtrue=simu(n,p,r,sqrt(noise))$mu
+result2=ComparMNAR_Multivariate(Xtrue,a,b,r,noise,Ns,10,nbsim)
