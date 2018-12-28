3a7fe99af3b80e7892ec0bce7c7e72da57d4592c
AudeSportisse <45034373+AudeSportisse@users.noreply.github.com>
2018-12-28 15:51:58 +0100

    Update Simulations.R

--- Simulations.R
+++ Simulations.R
@@ -1,3 +1,5 @@
+source('PlotSimulation.R')
+
 ######
 #Univariate
 ######
@@ -17,11 +19,13 @@ Xtrue=cbind.data.frame(dat,dat*1.1,dat*1.2,dat*1.3)
 
 ##Figure 2 (logit, MNAR)
 resultUnivariate=ComparMNAR_Univariate(Xtrue,a,b,r,noise,Ns,"logit","MNAR",nbsim)
+Plot(resultUnivariate)
 ##Figure 5 (logit, MAR)
 resultMARUnivariate=ComparMNAR_Univariate(Xtrue,a,b,r,noise,Ns,"logit","MAR",nbsim) 
+Plot(resultMARUnivariate,type="WithoutMask")
 #Figure 7 (probit, MNAR)
 resultProbitUnivariate=ComparMNAR_Univariate(Xtrue,a,b,r,noise,Ns,"probit","MNAR",nbsim) 
-
+Plot(resultProbitUnivariate,type="WithoutMask")
 
 ######
 #Bivariate
@@ -42,10 +46,12 @@ Ns=500
 res=simu(n,p,r,sqrt(noise))
 Xtrue=res$mu
 
-##Figure 6 (MAR)
-resultMARBivariate=ComparMNAR_Bivariate(Xtrue,a,b,a2,b2,r,noise,Ns,colbis=4,m1=10,m2=20,nbsim) 
 ##Figure 3 (MNAR)
 resultMNARBivariate=ComparMNAR_Bivariate(Xtrue,a,b,a2,b2,r,noise,Ns,colbis=4,m1=1,m2=4,nbsim)
+Plot(resultMNARBivariate)
+##Figure 6 (MAR)
+resultMARBivariate=ComparMNAR_Bivariate(Xtrue,a,b,a2,b2,r,noise,Ns,colbis=4,m1=10,m2=20,nbsim) 
+Plot(resultMARBivariate,type="WithoutMask")
 
 #######
 #Multivariate
@@ -65,4 +71,5 @@ Ns=500
 Xtrue=simu(n,p,r,sqrt(noise))$mu
 
 ##Figure 4 (for different noise values)
-result2=ComparMNAR_Multivariate(Xtrue,a,b,r,noise,Ns,10,nbsim)
+resultMNARMultivariate=ComparMNAR_Multivariate(Xtrue,a,b,r,noise,Ns,10,nbsim)
+Plot(resultMNARMultivariate)
